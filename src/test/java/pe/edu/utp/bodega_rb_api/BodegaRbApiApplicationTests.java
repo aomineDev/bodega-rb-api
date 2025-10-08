439957a4817e8f2d698528f682a25c10be34902b
aomine <aomine1745@gmail.com>
2025-10-08 12:26:04 -0500

    first commit

---
+++ src/test/java/pe/edu/utp/bodega_rb_api/BodegaRbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.bodega_rb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaRbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
