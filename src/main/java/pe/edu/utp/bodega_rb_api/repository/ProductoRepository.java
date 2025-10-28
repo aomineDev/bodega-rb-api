3a955d2bb600c6ed29f26d41b559018caa8362ab
aomine <aomine1745@gmail.com>
2025-10-28 06:25:26 -0500

    merge

--- src/main/java/pe/edu/utp/bodega_rb_api/repository/ProductoRepository.java
+++ src/main/java/pe/edu/utp/bodega_rb_api/repository/ProductoRepository.java
@@ -1,8 +1,10 @@
 package pe.edu.utp.bodega_rb_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pe.edu.utp.bodega_rb_api.model.Producto;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 }
