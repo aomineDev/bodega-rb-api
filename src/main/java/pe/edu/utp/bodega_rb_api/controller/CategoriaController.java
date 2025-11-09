35cf181d46c345932d698d657bbcaaa5fec099be
Jhordan <calixto4211@gmail.com>
2025-11-09 12:23:51 -0500

    controller categories

--- src/main/java/pe/edu/utp/bodega_rb_api/controller/CategoriaController.java
+++ src/main/java/pe/edu/utp/bodega_rb_api/controller/CategoriaController.java
@@ -42,6 +42,7 @@ public class CategoriaController {
 
   @PutMapping("/{id}")
   public ResponseEntity<Categoria> update(@PathVariable Integer id, @RequestBody Categoria entity) {
+    entity.setId(id);
     return ResponseEntity.ok(service.save(entity));
   }
 
