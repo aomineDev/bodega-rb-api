f6b0ab27d8a49669f72b0f17503d0219aa7c6c34
Jhordan <calixto4211@gmail.com>
2025-11-30 13:19:46 -0500

    descontar stock producto

--- src/main/java/pe/edu/utp/bodega_rb_api/repository/ProductoRepository.java
+++ src/main/java/pe/edu/utp/bodega_rb_api/repository/ProductoRepository.java
@@ -3,11 +3,21 @@ package pe.edu.utp.bodega_rb_api.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import jakarta.transaction.Transactional;
 import pe.edu.utp.bodega_rb_api.model.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
   List<Producto> findByCategoria_Id(Integer id);
+
+  @Transactional
+  @Modifying
+  @Query("UPDATE Producto p SET p.stock = p.stock - :cantidad WHERE p.id = :id")
+  void descontarStock(@Param("id") Integer id, @Param("cantidad") Double cantidad);
+
 }
